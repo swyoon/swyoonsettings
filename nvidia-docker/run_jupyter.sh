d0d43ae96fd4c5640f80d24538c2c5c534105bbe
swyoon <sangowong24yoon@gmail.com>
2019-04-01 16:52:42 +0900

    update dockerfile; add zshrc

--- nvidia-docker/run_jupyter.sh
+++ nvidia-docker/run_jupyter.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 tmux new -d -s "jupyter"
-tmux send-keys -t jupyter "/opt/conda/bin/jupyter lab --no-browser --ip='*' --NotebookApp.password='sha1:1cd70af36a42:54f332c52c30068c2de631f3a0ae367772803c0f' --notebook-dir=/opt/ --port=8888 --allow-root" ENTER
+tmux send-keys -t jupyter "/home/swyoon/.conda/envs/ml/bin/jupyter lab --no-browser --ip='0.0.0.0' --NotebookApp.password='sha1:1cd70af36a42:54f332c52c30068c2de631f3a0ae367772803c0f' --notebook-dir=/opt/ --port=8888 --allow-root" ENTER
